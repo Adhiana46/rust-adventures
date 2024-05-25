374be4984a2cdc41b9231e93b3a68c00d4119723
Adhiana Mastur <adhianamastur48@gmail.com>
2024-05-25 20:03:21 +0700

    variables

--- 01-day-1-morning/src/main.rs
+++ 01-day-1-morning/src/main.rs
@@ -1,7 +1,27 @@
 fn main() {
     hello_world();
+    variables();
 }
 
 fn hello_world() {
     println!("Hello, World!");
+}
+
+fn variables() {
+    // by default variable are immutable
+    let x: i32 = 32;
+
+    // to make it mutable add mut keyword
+    let mut y: i32 = 10;
+    y = y * x;
+
+    // unused variable show warning
+    let z: i32;
+
+    // to supress warning use _
+    let _xx: i32;
+
+    // println!("x: {}", x);
+    println!("x: {x}");
+    println!("y: {y}");
 }
