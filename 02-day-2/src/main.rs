1977aa7ae58824763021c46a0788cf4662e17209
Adhiana Mastur <adhianamastur48@gmail.com>
2024-05-26 10:03:31 +0700

    change mutual -> mutable :)

--- 02-day-2/src/main.rs
+++ 02-day-2/src/main.rs
@@ -2,8 +2,8 @@ fn main() {
     // References
     println!("=== References ===");
     shared_references();
-    mutual_borrow();
-    immutual_borrow();
+    mutable_borrow();
+    immutable_borrow();
     println!("\n");
 }
 
@@ -19,34 +19,34 @@ fn shared_references() {
 }
 
 // When you borrow a value mutably, you can modify it. However, you can only have one mutable borrow at a time to prevent data races.
-fn mutual_borrow() {
+fn mutable_borrow() {
     let mut a = 'A';
-    println!("mutual_borrow.a before: {}", a);
+    println!("mutable_borrow.a before: {}", a);
     let borrow_a: &mut char = &mut a;
     *borrow_a = 'C';
-    println!("mutual_borrow.a after: {}", a);
+    println!("mutable_borrow.a after: {}", a);
 
     // if uncomment, will result in compilation error
-    // because when passing a to println!, immutual borrow occurs
+    // because when passing a to println!, immutable borrow occurs
     // *borrow_a = 'D';
 }
 
 // When you borrow a value immutably, you cannot modify it. Multiple immutable borrows are allowed simultaneously.
-fn immutual_borrow() {
+fn immutable_borrow() {
     let mut a: char = 'A';
     let borrow_a: &char = &a;
 
-    println!("immutual_borrow.a before: {}", a);
-    println!("immutual_borrow.borrow_a before: {}", borrow_a);
+    println!("immutable_borrow.a before: {}", a);
+    println!("immutable_borrow.borrow_a before: {}", borrow_a);
 
     // change value of a
     // can't do that because it is borrowed by borrow_a
     // a = 'C';
 
-    // can't do this because it is immutual borrow
+    // can't do this because it is immutable borrow
     // *borrow_a = 'C';
 
-    // it will success, because it can have many immutual borrow
-    println!("immutual_borrow.a after: {}", a);
-    println!("immutual_borrow.borrow_a after: {}", borrow_a);
+    // it will success, because it can have many immutable borrow
+    println!("immutable_borrow.a after: {}", a);
+    println!("immutable_borrow.borrow_a after: {}", borrow_a);
 }
