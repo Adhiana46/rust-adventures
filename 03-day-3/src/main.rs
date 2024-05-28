7cbb389a684a52f7335b8959f479fb934fa313f4
Adhiana Mastur <adhianamastur48@gmail.com>
2024-05-28 09:47:14 +0700

    ownership, mutable reference and implement custom fmt::Debug

---
+++ 03-day-3/src/main.rs
@@ -0,0 +1,49 @@
+use std::fmt;
+
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age: u8,
+    mood: Mood,
+}
+
+enum Mood {
+    Happy,
+    Normal,
+    Depressed,
+}
+
+// implement custom fmt::Debug
+impl fmt::Debug for Mood {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Mood::Happy => write!(f, "😊"),
+            Mood::Normal => write!(f, "😐"),
+            Mood::Depressed => write!(f, "😔"),
+        }
+    }
+}
+
+fn print_person(p: &Person) {
+    println!(
+        "Hello my name is {}, my age is {} {:?}",
+        p.name, p.age, p.mood
+    );
+}
+
+fn make_person_depressed(p: &mut Person) {
+    p.mood = Mood::Depressed;
+}
+
+fn main() {
+    let mut adhiana: Person = Person {
+        name: String::from("Adhiana Mastur"),
+        age: 26,
+        mood: Mood::Happy,
+    };
+
+    print_person(&adhiana);
+    make_person_depressed(&mut adhiana);
+    print_person(&adhiana);
+    // println!("Person: {:?}", adhiana);
+}
